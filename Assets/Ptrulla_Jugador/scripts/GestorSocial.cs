3b1ec7d916ef091dff20aa5b2a49eed0fb0d05f1
irenetembras <tembrasdiazirene3@gmail.com>
2026-04-20 15:10:21 +0200

    Update GestorSocial.cs
    mensajes de formar equipo e informe

--- Assets/Ptrulla_Jugador/scripts/GestorSocial.cs
+++ Assets/Ptrulla_Jugador/scripts/GestorSocial.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent(typeof(BuzonMensajes))]
-// LA NUEVA CAPA 2: EXCLUSIVA PARA GESTIÓN FIPA Y SUBASTAS
 public class GestorSocial : MonoBehaviour
 {
     private BuzonMensajes miBuzon;
@@ -12,9 +11,9 @@ public class GestorSocial : MonoBehaviour
 
     [Header("Comunicación con el Cerebro")]
     public bool alarmaGeneralActivada = false;
-    public bool estaDisponible = true;   // El cerebro nos dirá si estamos ocupados
-    public bool tieneNuevaOrden = false; // Nosotros le diremos al cerebro si hay órdenes
-    public Vector3 coordenadaOrdenada;   // El GPS o punto táctico ya masticado
+    public bool estaDisponible = true;   
+    public bool tieneNuevaOrden = false; 
+    public Vector3 coordenadaOrdenada;   
 
     private struct Oferta { public GameObject guardia; public float distancia; }
     private List<Oferta> listaDeOfertas = new List<Oferta>();
@@ -28,7 +27,6 @@ public class GestorSocial : MonoBehaviour
 
     void Update()
     {
-        // Procesamos hasta  mensajes por frame
         int mensajesLeidos = 0;
         while (miBuzon != null && miBuzon.HayMensajesNuevos() && mensajesLeidos < 2)
         {
@@ -37,7 +35,6 @@ public class GestorSocial : MonoBehaviour
         }
     }
 
-    // El Cerebro llama a esta función cuando ve al jugador primero
     public void IniciarSubasta(Vector3 posLadron)
     {
         listaDeOfertas.Clear();
@@ -49,10 +46,17 @@ public class GestorSocial : MonoBehaviour
         {
             if (compañero != this)
             {
-                MensajeFIPA aviso = new MensajeFIPA(PerformativaFIPA.CFP, this.gameObject, compañero.gameObject, contenido);
-                compañero.GetComponent<BuzonMensajes>().RecibirMensaje(aviso);
+                // Escudo antierrores
+                BuzonMensajes suBuzon = compañero.GetComponent<BuzonMensajes>();
+                if (suBuzon != null)
+                {
+                    MensajeFIPA aviso = new MensajeFIPA(PerformativaFIPA.CFP, this.gameObject, compañero.gameObject, contenido);
+                    suBuzon.RecibirMensaje(aviso);
+                }
             }
         }
+        
+        Debug.Log(" [" + gameObject.name + "] ¡INICIO LA SUBASTA! 0.5 segundos para recibir ofertas...");
         StartCoroutine(CerrarSubastaYAsignar(posLadron));
     }
 
@@ -63,29 +67,39 @@ public class GestorSocial : MonoBehaviour
 
         if (mensaje.performativa == PerformativaFIPA.CFP)
         {
-            if (!estaDisponible) return; // Si el Cerebro dice que estamos ocupados, no pujamos
+            if (!estaDisponible) 
+            {
+                Debug.Log("[" + gameObject.name + "] Ignoro el aviso porque estoy ocupado.");
+                return; 
+            }
 
             Vector3 posLadron = ParsearCoordenadas(mensaje.contenido);
             float miDistancia = Vector3.Distance(miTransform.position, posLadron);
+            
             MensajeFIPA oferta = new MensajeFIPA(PerformativaFIPA.PROPOSE, this.gameObject, mensaje.emisor, miDistancia.ToString(System.Globalization.CultureInfo.InvariantCulture));
             mensaje.emisor.GetComponent<BuzonMensajes>().RecibirMensaje(oferta);
+            
+            Debug.Log(" [" + gameObject.name + "] Mando oferta: ¡Estoy a " + miDistancia + " metros!");
         }
         else if (mensaje.performativa == PerformativaFIPA.PROPOSE)
         {
             float distancia = float.Parse(mensaje.contenido, System.Globalization.CultureInfo.InvariantCulture);
             listaDeOfertas.Add(new Oferta { guardia = mensaje.emisor, distancia = distancia });
         }
-        // Juntamos el ACCEPT de la subasta y el INFORM del GPS porque ambos nos dan una meta
-        // Extraemos la lectura del buzón
         else if (mensaje.performativa == PerformativaFIPA.INFORM && mensaje.contenido == "ALARMA_ROBO")
         {
-            alarmaGeneralActivada = true; // ¡Nos avisaron del robo! Levantamos la bandera roja.
-            Debug.Log("📻 [" + gameObject.name + "] ¡Recibido CÓDIGO ROJO por radio! Voy a la emboscada.");
+            alarmaGeneralActivada = true; 
+            Debug.Log(" [" + gameObject.name + "] ¡CÓDIGO ROJO por radio! Voy a la emboscada.");
         }
         else if (mensaje.performativa == PerformativaFIPA.ACCEPT_PROPOSAL || (mensaje.performativa == PerformativaFIPA.INFORM && mensaje.contenido != "ALARMA_ROBO"))
         {
             coordenadaOrdenada = ParsearCoordenadas(mensaje.contenido);
             tieneNuevaOrden = true; 
+            Debug.Log(" [" + gameObject.name + "] ¡Fui aceptado en el equipo! Voy a cortar el paso.");
+        }
+        else if (mensaje.performativa == PerformativaFIPA.REJECT_PROPOSAL)
+        {
+            Debug.Log(" [" + gameObject.name + "] El líder me rechazó. Sigo patrullando.");
         }
     }
 
@@ -101,13 +115,20 @@ public class GestorSocial : MonoBehaviour
     private IEnumerator CerrarSubastaYAsignar(Vector3 posLadron)
     {
         yield return new WaitForSeconds(0.5f);
+        
+        Debug.Log(" [SUBASTA] Fin de tiempo para " + gameObject.name + ". Ofertas sobre la mesa: " + listaDeOfertas.Count);
+
         listaDeOfertas.Sort((a, b) => a.distancia.CompareTo(b.distancia));
 
         int guardiasAceptados = 0;
         int maxGuardias = 2; 
 
         GestorSensores sensores = GetComponent<GestorSensores>();
-        if(sensores == null || sensores.TransformJugador == null) yield break;
+        if(sensores == null || sensores.TransformJugador == null) 
+        {
+            Debug.LogError(" [ERROR TÁCTICO] " + gameObject.name + " perdió al jugador de vista demasiado rápido. Aborto subasta.");
+            yield break;
+        }
 
         Transform jugador = sensores.TransformJugador;
         Vector3 adelante = jugador.forward;
@@ -131,6 +152,8 @@ public class GestorSocial : MonoBehaviour
                 MensajeFIPA respuesta = new MensajeFIPA(PerformativaFIPA.ACCEPT_PROPOSAL, this.gameObject, oferta.guardia, contAceptar);
                 oferta.guardia.GetComponent<BuzonMensajes>().RecibirMensaje(respuesta);
                 guardiasAceptados++;
+                
+                Debug.Log(" [EL LÍDER " + gameObject.name + "] ASIGNA A: " + oferta.guardia.name + " al flanco de interceptación!");
             }
             else
             {
@@ -142,15 +165,17 @@ public class GestorSocial : MonoBehaviour
 
     public void DarAlarmaRobo()
     {
-        alarmaGeneralActivada = true; // Me doy por enterado yo también
-
+        alarmaGeneralActivada = true; 
         foreach (GestorSocial compañero in todosLosSociales)
         {
             if (compañero != this)
             {
-                // Mandamos un INFORM a todos diciendo la palabra mágica
-                MensajeFIPA aviso = new MensajeFIPA(PerformativaFIPA.INFORM, this.gameObject, compañero.gameObject, "ALARMA_ROBO");
-                compañero.GetComponent<BuzonMensajes>().RecibirMensaje(aviso);
+                BuzonMensajes suBuzon = compañero.GetComponent<BuzonMensajes>();
+                if (suBuzon != null)
+                {
+                    MensajeFIPA aviso = new MensajeFIPA(PerformativaFIPA.INFORM, this.gameObject, compañero.gameObject, "ALARMA_ROBO");
+                    suBuzon.RecibirMensaje(aviso);
+                }
             }
         }
     }
